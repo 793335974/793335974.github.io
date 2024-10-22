c6f00823a4acc7b7ee5bddf696a02af549662b01
wjt <793335974@qq.com>
2024-10-22 11:49:26 +0800

    Site updated: 2024-10-22 11:49:15

--- js/video.js
+++ js/video.js
@@ -1,13 +1,13 @@
 var videoUrls = 'https://jx.iqfk.top/api/sjsp.php';
 var myvideo = document.querySelector('.videoPlayer');
-function togglePlayPause(){
+function togglePlayPause() {
     if (myvideo) {
         myvideo.paused ? myvideo.play() : myvideo.pause();
     } else {
         console.error('没有找到具有 videoPlayer 类名的元素');
     }
 }
-function playNextVideo(){
+function playNextVideo() {
     if (myvideo) {
         myvideo.src = videoUrls;
         myvideo.load();
@@ -16,3 +16,24 @@ function playNextVideo(){
         console.error('没有找到具有 videoPlayer 类名的元素');
     }
 }
+
+const btnmei = document.getElementById('btnmei');
+const btnfei = document.getElementById('btnfei');
+const btnmuyu = document.getElementById('btnmuyu');
+const audio = document.getElementById('audio');
+
+btnmei.addEventListener('click', function () {
+    audio.src = './mp3/1.mp3'
+    audio.play();
+
+});
+
+btnfei.addEventListener('click', function () {
+    audio.src = './mp3/2.aac'
+    audio.play();
+});
+btnmuyu.addEventListener('click', function () {
+    btnmuyu.innerHTML = '功德+1'
+    audio.src = './mp3/3.mp3'
+    audio.play();
+});
