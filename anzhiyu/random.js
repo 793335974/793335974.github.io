0f02549e2efc412e496da2a3324d2f4ccf0ac678
unknown <793335974@qq.com>
2025-10-13 18:57:27 +0800

    Site updated: 2025-10-13 18:57:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/03/hello-world/"];function toRandomPost(){
+var posts=["2025/10/03/hello-world/","2025/10/13/system/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
