82975d769b31e83510f924a29212620fcbc3614c
unknown <793335974@qq.com>
2025-10-30 20:57:29 +0800

    Site updated: 2025-10-30 20:57:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/26/Nginx/","2025/10/03/hello-world/","2025/10/26/cloudserver/","2025/10/13/system/"];function toRandomPost(){
+var posts=["2025/10/26/cloudserver/","2025/10/26/Nginx/","2025/10/03/hello-world/","2025/10/13/system/","2025/10/29/mysql/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
