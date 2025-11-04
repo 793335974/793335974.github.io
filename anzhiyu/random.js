6c6efd37c93e92ee4e1ba1d9ed76e71033e2cd41
unknown <793335974@qq.com>
2025-11-04 18:31:53 +0800

    Site updated: 2025-11-04 18:31:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/26/Nginx/","2025/10/26/cloudserver/","2025/10/03/hello-world/","2025/11/02/jsproject/","2025/10/29/mysql/","2025/10/13/system/","2025/10/13/windowsCmd/"];function toRandomPost(){
+var posts=["2025/10/26/Nginx/","2025/10/26/cloudserver/","2025/11/02/jsproject/","2025/10/29/mysql/","2025/10/03/hello-world/","2025/11/04/js类的使用方法/","2025/10/13/system/","2025/11/04/学习ts/","2025/10/13/windowsCmd/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
